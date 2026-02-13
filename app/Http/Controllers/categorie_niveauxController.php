3dcba282b6fba4619839114eb9ada687edb9b990
Amadou coulibaly <amadoucoulibaly.865@gmail.com>
2026-02-13 00:08:25 -0100

    ajout de CRUD au categorie_niveaucontroller

--- app/Http/Controllers/categorie_niveauxController.php
+++ app/Http/Controllers/categorie_niveauxController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie_niveaux;
+use Exception;
 use Illuminate\Http\Request;
 
 class categorie_niveauxController extends Controller
@@ -11,7 +13,8 @@ class categorie_niveauxController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Categorie_niveaux::orderBy('created_at', 'desc')->paginate(10);
+        return view('categorie_niveaux.index', compact('categories'));
     }
 
     /**
@@ -19,7 +22,7 @@ class categorie_niveauxController extends Controller
      */
     public function create()
     {
-        //
+        return view('categorie_niveaux.create');
     }
 
     /**
@@ -27,38 +30,55 @@ class categorie_niveauxController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         $request->validate([
+        'categories_niveau' => 'required'
+        ]);
+
+        Categorie_niveaux::create($request->all());
+
+        return redirect()->route('categorie-niveaux.index')->with('success','Catégorie créée avec succès.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Categorie_niveaux $categorie_niveaux)
     {
-        //
+        return view('categorie_niveaux.show', compact('categorie_niveaux'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Categorie_niveaux $categorie_niveaux)
     {
-        //
+        return view('categorie_niveaux.edit', compact('categorie_niveaux'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Categorie_niveaux $categorie_niveaux)
     {
-        //
+        $request->validate([
+            'categories_niveau' => 'required|string|max:255'
+        ]);
+
+         $categorie_niveaux->update($request->all());
+
+         return redirect()->route('categorie_niveaux.index')->with('success', 'Catégorie mise à jour avec succès.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Categorie_niveaux $categorie_niveaux)
     {
-        //
+        try{
+            $categorie_niveaux->delete();
+            return redirect()->route('categorie_niveaux.index')->with('success', 'Catégorie supprimée avec succès.');
+        }catch(Exception $e){
+            return redirect()->route('categorie_niveaux.index')->with('error', 'Impossible de supprimer cette catégorie.');
+        }
     }
 }
